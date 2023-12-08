319aabaef6c105cca73a0e56b81c7580d8a23b79
Robin van Rooij <90185639+rarooij98@users.noreply.github.com>
2023-12-08 13:31:49 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 reportWebVitals();
